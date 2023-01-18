9367eac6e822dcf0c5e6deb6ff1b5e7c9c497f3a
Alemi <2004alemi@gmail.com>
2023-01-18 10:20:51 +0100

    Probando clase

--- src/calculadora/Principal.java
+++ src/calculadora/Principal.java
@@ -4,5 +4,8 @@ public class Principal {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		calculadora miCalc = new calculadora();
+		System.out.println(miCalc.sumar(5,3));
+		System.out.println(miCalc.restar(5,3));
+
 	}
 }
