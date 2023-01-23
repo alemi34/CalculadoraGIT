4744885faceb2d6bbb82c761bd656199a1893cb2
alemi34 <83096463+alemi34@users.noreply.github.com>
2023-01-23 10:18:47 +0100

    Update Principal.java

--- src/calculadora/Principal.java
+++ src/calculadora/Principal.java
@@ -7,6 +7,7 @@ public class Principal {
 		System.out.println(miCalc.sumar(5,3));
 		System.out.println(miCalc.restar(5,3));
 		System.out.println(miCalc.multiplicar(5,3));
+		System.out.println(miCalc.dividir(5,3));
 
 
 	}
